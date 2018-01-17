5957ca1ec8a1ce8b59dfa36c4a19aec4f31da7ca
jordano <jordanomoraesgoncalves@gmail.com>
2018-01-17 20:37:24 -0200

    Resource de categoria

--- src/main/java/br/com/jordano/domain/CategoriaResource.java
+++ src/main/java/br/com/jordano/domain/CategoriaResource.java
@@ -1,7 +1,18 @@
 package br.com.jordano.domain;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by jordano on 17/01/2018.
  */
+@RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+    @GetMapping
+    public String listar(){
+        return "OK";
+    }
 }
